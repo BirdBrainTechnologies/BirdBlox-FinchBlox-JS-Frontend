7c6f061af1017bad135c4d35f8158005a7ce2825
Terence Sun <tsun1215@gmail.com>
2017-04-10 18:25:52 -0400

    Updated portslot to have default option

--- BlockParts/Slot/PortSlot.js
+++ BlockParts/Slot/PortSlot.js
@@ -1,16 +1,17 @@
 function PortSlot(parent, maxPorts) {
 	DropSlot.call(this, parent, Slot.snapTypes.none);
 	this.maxPorts = maxPorts;
+	this.setSelectionData();
+    for(let portNum = 1; portNum <= this.maxPorts; portNum++) {
+        this.addOption(portNum.toString(), new SelectionData(portNum));
+    }
+    this.setSelectionData("1",new SelectionData(1));
 }
 
 PortSlot.prototype = Object.create(DropSlot.prototype);
 PortSlot.prototype.constructor = PortSlot;
 
 PortSlot.prototype.populateList=function(){
-    this.clearOptions();
-    for(let portNum = 1; portNum <= this.maxPorts; portNum++) {
-        this.addOption(portNum.toString(), new SelectionData(portNum));
-    }
 };
 PortSlot.prototype.duplicate=function(parentBlock){
     let clone = new PortSlot(parentBlock);
