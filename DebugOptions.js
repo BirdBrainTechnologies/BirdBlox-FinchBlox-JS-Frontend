29accd4a1f5b4a7bcf74dfb3b7a6f48f8b0d7689
Tom Wildenhain <101010tw42@gmail.com>
2017-05-15 23:19:35 -0400

    - Added DebugOptions.js file for easier debugging - Implemented PortSlots for HB blocks - Increased size of blocks and buttons

---
+++ DebugOptions.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function DebugOptions(){
+	var DO = DebugOptions;
+	DO.enabled = true;
+
+	DO.mouse = false;
+	DO.addVirtualHB = true;
+	DO.addVirtualFlutter = false;
+
+	if(DO.enabled){
+		DO.applyConstants();
+	}
+}
+DebugOptions.applyConstants = function(){
+	var DO = DebugOptions;
+	if(DO.mouse){
+		TouchReceiver.mouse = true;
+	}
+};
+DebugOptions.applyActions = function(){
+	var DO = DebugOptions;
+	if(DO.addVirtualHB){
+		let virHB = new Hummingbird("Virtual HB");
+		virHB.connect();
+	}
+};
