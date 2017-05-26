fe6af0f0703f4daa120d2842f5f513d3dd6eecef
Tom Wildenhain <101010tw42@gmail.com>
2017-05-25 21:55:45 -0400

    Added delete option to block context menu

--- TouchReceiver.js
+++ TouchReceiver.js
@@ -264,8 +264,10 @@ TouchReceiver.touchmove=function(e){
 	var TR=TouchReceiver;
 	if(TR.touchDown&&(TR.hasMovedOutsideThreshold(e) || TR.dragging)){
 		TR.dragging = true;
-		GuiElements.overlay.close(true);
-		TR.longTouch = false;
+		if(TR.longTouch) {
+			GuiElements.overlay.close();
+			TR.longTouch = false;
+		}
 		if(TR.zooming){
 			//If we are currently zooming, we update the zoom.
 			if(e.touches.length < 2){
