8e9916eafc3b9ae5b67adb689aa41e397ee2fde9
Tom Wildenhain <101010tw42@gmail.com>
2017-07-27 19:26:38 -0400

    Added documentation to variables

--- all.js
+++ all.js
@@ -1015,74 +1015,125 @@ Timer.prototype.tick = function(){
 Timer.prototype.isRunning = function(){
 	return this.updateTimer != null;
 };
-function Variable(name, data){
-	this.name=name;
-	this.data=data;
-	if(this.data==null){
-		this.data=new NumData(0);
+/**
+ * Represents a user-created Variable that is part of the current project.  A variable can hold NumData, StringData,
+ * and BoolData, but not ListData or SelectionData.
+ * @param {string} name - The name of the variable
+ * @param {Data} [data] - The initial data in the variable
+ * @constructor
+ */
+function Variable(name, data) {
+	this.name = name;
+	this.data = data;
+	if (this.data == null) {
+		this.data = new NumData(0);
 	}
 	CodeManager.addVariable(this);
 }
-Variable.prototype.getName=function(){
+
+/**
+ * Gets the name of the Variable
+ * @return {string}
+ */
+Variable.prototype.getName = function() {
 	return this.name;
 };
-Variable.prototype.getSelectionData = function(){
+
+/**
+ * Returns SelectionData for the Variable to be selected from a DropSlot
+ * @return {SelectionData}
+ */
+Variable.prototype.getSelectionData = function() {
 	return new SelectionData(this.name, this);
 };
-Variable.prototype.getData=function(){
+
+/**
+ * Gets the data stored in the Variable
+ * @return {Data}
+ */
+Variable.prototype.getData = function() {
 	return this.data;
 };
-Variable.prototype.setData=function(data){
-	this.data=data;
+
+/**
+ * Sets the data in the Variable
+ * @param data
+ */
+Variable.prototype.setData = function(data) {
+	this.data = data;
 };
-Variable.prototype.remove=function(){
-	this.data=null;
+
+/**
+ * Removes the Variable from the CodeManager
+ */
+Variable.prototype.remove = function() {
+	this.data = null;
 	CodeManager.removeVariable(this);
 };
-Variable.prototype.createXml=function(xmlDoc) {
-	var variable = XmlWriter.createElement(xmlDoc, "variable");
+
+/**
+ * Exports information about the Variable to XML
+ * @param {Document} xmlDoc - The document to write to
+ * @return {Node} - The node for the Variable
+ */
+Variable.prototype.createXml = function(xmlDoc) {
+	const variable = XmlWriter.createElement(xmlDoc, "variable");
 	XmlWriter.setAttribute(variable, "name", this.name);
 	variable.appendChild(this.data.createXml(xmlDoc));
 	return variable;
 };
-Variable.importXml=function(variableNode){
-	var name=XmlWriter.getAttribute(variableNode,"name");
-	if(name!=null){
-		var dataNode=XmlWriter.findSubElement(variableNode,"data");
-		var data=new NumData(0);
-		if(dataNode!=null){
-			var newData=Data.importXml(dataNode);
-			if(newData!=null){
-				data=newData;
+
+/**
+ * Creates a Variable from XML
+ * @param {Node} variableNode
+ * @return {Variable|null}
+ */
+Variable.importXml = function(variableNode) {
+	const name = XmlWriter.getAttribute(variableNode, "name");
+	if (name != null) {
+		const dataNode = XmlWriter.findSubElement(variableNode, "data");
+		let data = new NumData(0);
+		if (dataNode != null) {
+			const newData = Data.importXml(dataNode);
+			if (newData != null) {
+				data = newData;
 			}
 		}
-		return new Variable(name,data);
+		return new Variable(name, data);
 	}
+	return null
 };
-Variable.prototype.rename=function(){
-	var callbackFn=function(cancelled,response){
-		if(!cancelled&&CodeManager.checkVarName(response)){
-			callbackFn.variable.name=response;
+
+/**
+ * Prompts the user to rename the variable
+ */
+Variable.prototype.rename = function() {
+	const callbackFn = function(cancelled, response) {
+		if (!cancelled && CodeManager.checkVarName(response)) {
+			callbackFn.variable.name = response;
 			CodeManager.renameVariable(callbackFn.variable);
 		}
 	};
-	callbackFn.variable=this;
-	DialogManager.showPromptDialog("Rename variable","Enter variable name",this.name,true,callbackFn);
+	callbackFn.variable = this;
+	DialogManager.showPromptDialog("Rename variable", "Enter variable name", this.name, true, callbackFn);
 };
-Variable.prototype.delete=function(){
-	if(CodeManager.checkVariableUsed(this)) {
-		var callbackFn = function (response) {
-			if (response == "2") {
+
+/**
+ * Prompts the user to delete the Variable
+ */
+Variable.prototype.delete = function() {
+	if (CodeManager.checkVariableUsed(this)) {
+		const callbackFn = function(response) {
+			if (response === "2") {
 				callbackFn.variable.remove();
 				CodeManager.deleteVariable(callbackFn.variable);
 			}
 		};
 		callbackFn.variable = this;
-		var question = "Are you sure you would like to delete the variable \"" + this.name + "\"? ";
+		let question = "Are you sure you would like to delete the variable \"" + this.name + "\"? ";
 		question += "This will delete all copies of this block.";
 		DialogManager.showChoiceDialog("Delete variable", question, "Don't delete", "Delete", true, callbackFn);
-	}
-	else{
+	} else {
 		this.remove();
 		CodeManager.deleteVariable(this);
 	}
